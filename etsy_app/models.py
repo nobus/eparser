1cb8b9af549d166e87664a1268f6cde8255147f2
nobus <meganobus@gmail.com>
2019-12-18 18:23:12 +0300

    Add CeleryStat

--- etsy_app/models.py
+++ etsy_app/models.py
@@ -1,3 +1,14 @@
+# -*- coding: utf-8 -*-
+
 from django.db import models
 
-# Create your models here.
+class CeleryStat(models.Model):
+    task_id = models.CharField(primary_key=True, max_length=64)
+    task_name = models.CharField(max_length=64)
+    task_status = models.CharField(max_length=8, default='PENDING')
+    oid = models.CharField(max_length=64)
+    limit = models.PositiveIntegerField(default=0)
+    counter = models.PositiveIntegerField(default=0)
+
+    start_datetime = models.DateTimeField(auto_now=False)
+    end_datetime = models.DateTimeField(auto_now=False)
