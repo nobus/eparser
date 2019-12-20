bc19cef5abacfdb6e862e23431fad648a357db73
nobus <meganobus@gmail.com>
2019-12-20 09:32:11 +0300

    Add Listing

--- etsy_app/models.py
+++ etsy_app/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 class CeleryStat(models.Model):
     task_id = models.CharField(primary_key=True, max_length=64)
@@ -12,3 +13,62 @@ class CeleryStat(models.Model):
 
     start_datetime = models.DateTimeField(auto_now=False)
     end_datetime = models.DateTimeField(auto_now=False)
+
+class Listing(models.Model):
+    """
+    https://www.etsy.com/developers/documentation/reference/listing#section_fields
+    """
+
+    listing_id = models.BigIntegerField(primary_key=True)
+
+    state = models.CharField(max_length=16, null=True, blank=True, default=None)
+    user_id = models.BigIntegerField(null=True, blank=True, default=None)
+    category_id = models.BigIntegerField(null=True, blank=True, default=None)
+    title = models.TextField(null=True, blank=True, default=None)
+    description = models.TextField(null=True, blank=True, default=None)
+    creation_tsz = models.FloatField(null=True, blank=True, default=None)
+    ending_tsz = models.FloatField(null=True, blank=True, default=None)
+    original_creation_tsz = models.FloatField(null=True, blank=True, default=None)
+    last_modified_tsz = models.FloatField(null=True, blank=True, default=None)
+    price = models.FloatField(null=True, blank=True, default=None)
+    currency_code = models.CharField(max_length=3, default='')
+    quantity = models.PositiveIntegerField(null=True, blank=True, default=None)
+    sku = ArrayField(models.CharField(max_length=2), blank=True, null=True)
+    tags = ArrayField(models.TextField(), blank=True, null=True)
+    category_path = ArrayField(models.TextField(), blank=True, null=True)
+    category_path_ids = ArrayField(models.PositiveIntegerField(), blank=True, null=True)
+    materials = ArrayField(models.TextField(), blank=True, null=True)
+    shop_section_id = models.PositiveIntegerField(null=True, blank=True, default=None)
+    featured_rank = models.IntegerField(null=True, blank=True, default=None)
+    state_tsz = models.PositiveIntegerField(null=True, blank=True, default=None)
+    url = models.URLField(max_length=256, null=True, blank=True, default=None)
+    views = models.PositiveIntegerField(null=True, blank=True, default=None)
+    num_favorers = models.PositiveIntegerField(null=True, blank=True, default=None)
+    shipping_template_id = models.BigIntegerField(null=True, blank=True, default=None)
+    processing_min = models.PositiveIntegerField(null=True, blank=True, default=None)
+    processing_max = models.PositiveIntegerField(null=True, blank=True, default=None)
+    who_made = models.CharField(max_length=16, null=True, blank=True, default=None)
+    is_supply = models.NullBooleanField()
+    when_made = models.CharField(max_length=16, null=True, blank=True, default=None)
+    item_weight = models.PositiveIntegerField(null=True, blank=True, default=None)
+    item_weight_unit = models.CharField(max_length=2)
+    item_length = models.PositiveIntegerField(null=True, blank=True, default=None)
+    item_width = models.PositiveIntegerField(null=True, blank=True, default=None)
+    item_height = models.PositiveIntegerField(null=True, blank=True, default=None)
+    item_dimensions_unit = models.CharField(max_length=2, null=True, blank=True, default=None)
+    is_private = models.NullBooleanField()
+    recipient = models.CharField(max_length=16, null=True, blank=True, default=None)
+    occasion = models.CharField(max_length=3, null=True, blank=True, default=None)
+    style = ArrayField(models.TextField(), blank=True, null=True)
+    non_taxable = models.NullBooleanField()
+    is_customizable = models.NullBooleanField()
+    is_digital = models.NullBooleanField()
+    file_data = models.TextField(null=True, blank=True, default=None)
+    should_auto_renew = models.NullBooleanField()
+    language = models.CharField(max_length=8, null=True, blank=True, default=None)
+    has_variations = models.NullBooleanField()
+    taxonomy_id = models.PositiveIntegerField(null=True, blank=True, default=None)
+    taxonomy_path = ArrayField(models.TextField(), blank=True, null=True)
+    suggested_taxonomy_id = models.PositiveIntegerField(null=True, blank=True, default=None)
+    used_manufacturer = models.NullBooleanField()
+    is_vintage = models.NullBooleanField()
