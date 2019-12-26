5fd260749004269ece3c710668a71f6af0b7fb55
nobus <meganobus@gmail.com>
2019-12-26 08:24:33 +0300

    Add PriceSerializer

--- etsy_app/serializers.py
+++ etsy_app/serializers.py
@@ -1,10 +1,20 @@
 # -*- coding: utf-8 -*-
 from rest_framework import serializers
 
-from etsy_app.models import CeleryStat
+from etsy_app.models import CeleryStat, Listing, Offering
 
 
 class CeleryStatSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = CeleryStat
         fields = '__all__'
+
+
+class PriceSerializer(serializers.Serializer):
+    listing_id = serializers.IntegerField(read_only=True)
+    price = serializers.IntegerField(read_only=True)
+    url = serializers.URLField(read_only=True)
+    currency_code = serializers.CharField(max_length=3, read_only=True)
+
+    class Meta:
+        model = Listing
