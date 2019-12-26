3578b5b886dafb125878d2b2c940fa636fc2f8eb
nobus <meganobus@gmail.com>
2019-12-26 08:26:15 +0300

    Register url for PriceViewSet

--- etsy_app/urls.py
+++ etsy_app/urls.py
@@ -3,10 +3,12 @@ from django.urls import path, include
 
 from rest_framework.routers import DefaultRouter
 
-from etsy_app.views import CeleryStatViewSet
+from etsy_app.views import CeleryStatViewSet, PriceViewSet
 
 router = DefaultRouter()
 router.register(r'celery_stat', CeleryStatViewSet)
+router.register(r'price', PriceViewSet)
+
 
 urlpatterns = [
     path('', include(router.urls)),
