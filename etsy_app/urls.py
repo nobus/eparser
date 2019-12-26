f96f962b4d7c99178a38fcdb84acc2fe63b4feab
nobus <meganobus@gmail.com>
2019-12-26 16:16:58 +0300

    Add HistogramView

--- etsy_app/urls.py
+++ etsy_app/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 
 from rest_framework.routers import DefaultRouter
 
-from etsy_app.views import CeleryStatViewSet, PriceViewSet
+from etsy_app.views import CeleryStatViewSet, PriceViewSet, HistogramView
 
 router = DefaultRouter()
 router.register(r'celery_stat', CeleryStatViewSet)
@@ -11,5 +11,6 @@ router.register(r'price', PriceViewSet)
 
 
 urlpatterns = [
+    path('hgram', HistogramView.as_view()),
     path('', include(router.urls)),
 ]
--- etsy_app/views.py
+++ etsy_app/views.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
+import numpy as np
+
+from django.views import View
+from django.http import JsonResponse
 from django.shortcuts import render
 
 from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-from etsy_app.models import CeleryStat, Listing, Offering
+from etsy_app.models import CeleryStat, Listing
 from etsy_app.serializers import CeleryStatSerializer, PriceSerializer
 
 class CeleryStatViewSet(viewsets.ReadOnlyModelViewSet):
@@ -18,3 +22,27 @@ class PriceViewSet(viewsets.ReadOnlyModelViewSet):
 
     def list(self, request):
         return super().list(request)
+
+class HistogramView(View):
+    def get(self, request):
+        nbins = int(request.GET.get('nbins', 100))
+        minv = int(request.GET.get('minv', -1))
+        maxv = int(request.GET.get('maxv', -1))
+        currency = request.GET.get('currency', None)
+
+        prices = Listing.objects.values_list('price', flat=True).exclude(price__isnull=True)
+
+        if minv >=0 and maxv >=0:
+            prices = prices.filter(price__range=[minv, maxv])
+
+        if currency:
+            prices = prices.filter(currency_code=currency)
+
+        h = np.histogram(prices, bins=nbins)
+
+        ret = {
+            'bins': h[0].tolist(),
+            'edges': h[1].tolist(),
+        }
+
+        return JsonResponse(ret)
