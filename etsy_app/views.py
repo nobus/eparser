90211d5ff6d4403efc95f18d045a1c09d8618986
nobus <meganobus@gmail.com>
2019-12-26 08:25:06 +0300

    Add experimental PriceViewSet

--- etsy_app/views.py
+++ etsy_app/views.py
@@ -2,10 +2,19 @@
 from django.shortcuts import render
 
 from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-from etsy_app.models import CeleryStat
-from etsy_app.serializers import CeleryStatSerializer
+from etsy_app.models import CeleryStat, Listing, Offering
+from etsy_app.serializers import CeleryStatSerializer, PriceSerializer
 
 class CeleryStatViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = CeleryStat.objects.all()
     serializer_class = CeleryStatSerializer
+
+class PriceViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Listing.objects.values('listing_id', 'price', 'currency_code', 'url')
+    serializer_class = PriceSerializer
+
+    def list(self, request):
+        return super().list(request)
